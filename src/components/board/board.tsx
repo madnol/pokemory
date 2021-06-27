c266557c60929385cf041aba98a898e51374585c
manuel <desole.manuel@gmail.com>
2021-06-27 12:51:02 +0200

    change description

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -44,7 +44,7 @@ export const Board: FunctionComponent<Props> = ({
         if (check) {
 
             setFounded([...founded, ...pairing])
-            console.log('founded')
+
         }
 
     }
